3d8535eaa3093002457b0a59c5bda41fac868d80
Luis Guillermo <abg.lgmm@gmail.com>
2023-11-22 18:28:07 +0000

    login

--- src/front/js/store/flux.js
+++ src/front/js/store/flux.js
@@ -21,6 +21,24 @@ const getState = ({ getStore, getActions, setStore }) => {
 				getActions().changeColor(0, "green");
 			},
 
+			login: async (email, password) => {
+				
+					// fetching data from the backend
+					const options = {method: "POST",
+						             headers: {"Content-Type": "application/json"},
+						             body: JSON.stringify({"email": email, "password": password})}
+					const response = await fetch(process.env.BACKEND_URL + "/api/login", options)
+					if (!response.ok) {
+						console.log("Error loading message from backend", response.status, response.statusText);
+						return {status: response.status, statusText: response.statusText}
+					}
+					const data = await response.json()
+					         localStorage.setItem("token", data.access_token)
+					setStore({ email: email })
+					// don't forget to return something, that is how the async resolves
+					return data;
+					},
+
 			getMessage: async () => {
 				try{
 					// fetching data from the backend
